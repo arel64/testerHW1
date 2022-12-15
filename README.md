b600936684f509f3b4e422ae50d03eb2e805eb73
Arel Sharon <arel.sharo@gmail.com>
2022-12-16 00:32:50 +0200

    fin

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # testerHW1
+
+Clone Repo, Drag kmeans.c into the same directoy as t.sh
+run:
+chmod +x t.sh
+./t.sh
+
+Valid Output Example:
+Test Success InvalidClusterLow1.txt
+Test Success InvalidClusterHigh1.txt
+Test Success InvalidClusterWithIter1.txt
+Test Success InvalidClusterWithValidIter1.txt
+Test Success InvalidIter1.txt
+Test Success InvalidIterHigh1.txt
+Test Success InvalidIterHighNonNum1.txt
+Test Success InvalidClusterNonNum1.txt
+Test Success InvalidClusterLow2.txt
+Test Success InvalidClusterHigh2.txt
+Test Success InvalidClusterWithIter2.txt
+Test Success InvalidClusterWithValidIter2.txt
+Test Success InvalidIter2.txt
+Test Success InvalidIterHigh2.txt
+Test Success InvalidIterHighNonNum2.txt
+Test Success InvalidClusterNonNum2.txt
+Test Success Valid1.txt
+Test Success Valid2.txt
+Test Success Valid3.txt
+Good Output 1
+Good Output 2
+Good Output 3
+
+Any other output should be considered invalid
+
+
+Features:
+Checks many edge cases for input and whether valgrind detects leaked memory for it
+This includes ~20 tests for
+*Valid Runs with 3 parameters
+*Valid Runs with 2 parameters
+*Invalid Runs with 2 paramters
+*Invalid Runs with 3 parameters
+*Any leaked memory will result in test fail
+
+*This will also check that all 3 inputs provided return the output provided
+*This program checks the error codes for valgrind, which means tests will fail if you leak memory in any way
+*This script generates valgrind logs for each test, look at the /logs folder for specific errors
+*Maybe something else I forgot
+
+Limitations:
+*If you return 1 but return an incorrect error message(for example typo in the error cluster message and return 1) the test will falsly pass.
+*Maybe something else I forgot
+
+
+******
+Use at you own risk, I can't say for sure this will work or detect any bugs
+******
--- t.sh
+++ t.sh
@@ -8,7 +8,7 @@ function run_valgrind_command() {
     echo -n "Test Success "
 	eval "grep -oP '(?<=log-file=logs/)[^ ]*' <<< '$valgrind_command'"
   else
-	echo "Test Fail, for parmeters"
+	echo "Test Fail(Mem Leak or unexpected error code), for parmeters"
     eval "grep -oP '(?<=kmeans ).*$' <<< '$valgrind_command'"  
   fi
 
@@ -23,28 +23,28 @@ fi
 rm -rf logs
 mkdir -p logs
 valgrind_bad_commands=(
-	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidClusterLow1.txt --track-origins=yes --error-exitcode=2 ./kmeans 0 <input/input_1.txt"
-	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidClusterHigh1.txt --track-origins=yes --error-exitcode=2 ./kmeans 100000 <input/input_1.txt"
-	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidClusterWithIter1.txt --track-origins=yes --error-exitcode=2 ./kmeans 100000 100000 <input/input_1.txt"
-	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidClusterWithValidIter1.txt --track-origins=yes --error-exitcode=2 ./kmeans 100000 100 <input/input_1.txt"
-	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidIter1.txt --track-origins=yes --error-exitcode=2 ./kmeans 10 0 <input/input_1.txt"
-	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidIterHigh1.txt --track-origins=yes --error-exitcode=2 ./kmeans 10 1000 <input/input_1.txt"
-	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidIterHighNonNum1.txt --track-origins=yes --error-exitcode=2 ./kmeans 10 1a0 <input/input_1.txt"
-	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidClusterNonNum1.txt --track-origins=yes --error-exitcode=2 ./kmeans 1a0 10 <input/input_1.txt"
+	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidClusterLow1.txt --track-origins=yes --error-exitcode=2 --errors-for-leak-kinds=all ./kmeans 0 <input/input_1.txt"
+	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidClusterHigh1.txt --track-origins=yes --error-exitcode=2 --errors-for-leak-kinds=all ./kmeans 100000 <input/input_1.txt"
+	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidClusterWithIter1.txt --track-origins=yes --error-exitcode=2 --errors-for-leak-kinds=all ./kmeans 100000 100000 <input/input_1.txt"
+	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidClusterWithValidIter1.txt --track-origins=yes --error-exitcode=2 --errors-for-leak-kinds=all ./kmeans 100000 100 <input/input_1.txt"
+	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidIter1.txt --track-origins=yes --error-exitcode=2 --errors-for-leak-kinds=all ./kmeans 10 0 <input/input_1.txt"
+	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidIterHigh1.txt --track-origins=yes --error-exitcode=2 --errors-for-leak-kinds=all ./kmeans 10 1000 <input/input_1.txt"
+	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidIterHighNonNum1.txt --track-origins=yes --error-exitcode=2 --errors-for-leak-kinds=all ./kmeans 10 1a0 <input/input_1.txt"
+	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidClusterNonNum1.txt --track-origins=yes --error-exitcode=2 --errors-for-leak-kinds=all ./kmeans 1a0 10 <input/input_1.txt"
 	
-	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidClusterLow2.txt --track-origins=yes --error-exitcode=2 ./kmeans 0 <input/input_2.txt"
-	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidClusterHigh2.txt --track-origins=yes --error-exitcode=2 ./kmeans 100000 <input/input_2.txt"
-	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidClusterWithIter2.txt --track-origins=yes --error-exitcode=2 ./kmeans 100000 100000 <input/input_2.txt"
-	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidClusterWithValidIter2.txt --track-origins=yes --error-exitcode=2 ./kmeans 100000 100 <input/input_2.txt"
-	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidIter2.txt --track-origins=yes --error-exitcode=2 ./kmeans 10 0 <input/input_2.txt"
-	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidIterHigh2.txt --track-origins=yes --error-exitcode=2 ./kmeans 10 1000 <input/input_2.txt"
-	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidIterHighNonNum2.txt --track-origins=yes --error-exitcode=2 ./kmeans 10 1a0 <input/input_2.txt"
-	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidClusterNonNum2.txt --track-origins=yes --error-exitcode=2 ./kmeans 1a0 10 <input/input_2.txt"
+	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidClusterLow2.txt --track-origins=yes --error-exitcode=2 --errors-for-leak-kinds=all ./kmeans 0 <input/input_2.txt"
+	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidClusterHigh2.txt --track-origins=yes --error-exitcode=2 --errors-for-leak-kinds=all ./kmeans 100000 <input/input_2.txt"
+	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidClusterWithIter2.txt --track-origins=yes --error-exitcode=2 --errors-for-leak-kinds=all ./kmeans 100000 100000 <input/input_2.txt"
+	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidClusterWithValidIter2.txt --track-origins=yes --error-exitcode=2 --errors-for-leak-kinds=all ./kmeans 100000 100 <input/input_2.txt"
+	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidIter2.txt --track-origins=yes --error-exitcode=2 --errors-for-leak-kinds=all ./kmeans 10 0 <input/input_2.txt"
+	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidIterHigh2.txt --track-origins=yes --error-exitcode=2 --errors-for-leak-kinds=all ./kmeans 10 1000 <input/input_2.txt"
+	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidIterHighNonNum2.txt --track-origins=yes --error-exitcode=2 --errors-for-leak-kinds=all ./kmeans 10 1a0 <input/input_2.txt"
+	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/InvalidClusterNonNum2.txt --track-origins=yes --error-exitcode=2 --errors-for-leak-kinds=all ./kmeans 1a0 10 <input/input_2.txt"
 )
 valgrind_good_commands=(
-	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/Valid1.txt --track-origins=yes --error-exitcode=2 ./kmeans 3 100 <input/input_1.txt"
-	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/Valid2.txt --track-origins=yes --error-exitcode=2 ./kmeans 7 <input/input_2.txt"
-	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/Valid3.txt --track-origins=yes --error-exitcode=2 ./kmeans 15 300 <input/input_3.txt"
+	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/Valid1.txt --track-origins=yes --error-exitcode=2 --errors-for-leak-kinds=all ./kmeans 3 100 <input/input_1.txt"
+	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/Valid2.txt --track-origins=yes --error-exitcode=2 --errors-for-leak-kinds=all ./kmeans 7 <input/input_2.txt"
+	"valgrind --leak-check=full --show-reachable=yes --log-file=logs/Valid3.txt --track-origins=yes --error-exitcode=2 --errors-for-leak-kinds=all ./kmeans 15 300 <input/input_3.txt"
 )
 
 #memory test
